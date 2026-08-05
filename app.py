09ef9be3ea2a37d5cafeb4437b5b0d936bb570f3
maxs0106 <285907516+maxs0106@users.noreply.github.com>
2026-08-06 03:19:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -677,10 +677,12 @@ def main():
             
             with c_f2:
                 st.caption("表示Tier範囲 ")
-                ct1, ct2 = st.columns(2)
+                ct1, ct2, ct3 = st.columns(3)
                 with ct1:
                     min_t = st.number_input("",min_value=1, max_value=9, value=1, step=1, help="1: Tier I 〜 9: ★")
                 with ct2:
+                    st.subheader("～")
+                with ct3:
                     max_t = st.number_input("",min_value=1, max_value=9, value=9, step=1, help="1: Tier I 〜 9: ★")
             
             # マスク処理
